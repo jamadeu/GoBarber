3b4a7517dd0dc53ae2066e758c4022da43e616e8
jamadeu <jean.amadeu2@gmail.com>
2020-02-21 05:51:54 -0300

    Auth config

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 
+import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
@@ -24,8 +25,8 @@ class SessionController {
         name,
         email,
       },
-      token: jwt.sign({ id }, 'e921e554ef344241b7270a9ce954024b', {
-        expiresIn: '7d',
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
       }),
     });
   }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'e921e554ef344241b7270a9ce954024b',
+  expiresIn: '7d',
+};
