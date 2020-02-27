1585945cb6442bd80d2381e46b9b3847a462b633
jamadeu <jean.amadeu2@gmail.com>
2020-02-27 05:12:02 -0300

    User avatar

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 
+import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
@@ -16,8 +17,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
