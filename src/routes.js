a5013bb9862c9ea38cd440ae0842177db6174421
jamadeu <jean.amadeu2@gmail.com>
2020-02-27 05:33:05 -0300

    Provider list

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
@@ -16,6 +17,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
