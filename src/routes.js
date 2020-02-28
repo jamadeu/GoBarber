db0033d76fc82aef5f59ffd0690c7694eea285c3
jamadeu <jean.amadeu2@gmail.com>
2020-02-28 05:28:34 -0300

    List schedule

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
@@ -24,5 +25,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
