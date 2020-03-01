85b2b8fb4ecd0626999eb546996e53a52c5fdecf
jamadeu <jean.amadeu2@gmail.com>
2020-03-01 10:54:52 -0300

    Email templates

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -118,6 +118,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -146,7 +151,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Appointment canceled',
-      text: 'You have a new cancellation',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
